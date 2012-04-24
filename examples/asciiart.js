4d5eb2129d51a6fcd8dbd97b13b4d583911b6700
Frederic Junod <frederic.junod@camptocamp.com>
2012-04-24 15:32:04 +0200

    Add MapBox Streets layer

--- examples/asciiart.js
+++ examples/asciiart.js
@@ -8,8 +8,8 @@ function init() {
     var osm_ascii = new OpenLayers.Layer.OSM("Mapnik ASCII", null, {
         tileClass: OpenLayers.Tile.ASCII
     });
-
     var osm = new OpenLayers.Layer.OSM("Mapnik");
+
     var bing_ascii = new OpenLayers.Layer.Bing({
         name: "Bing road ASCII",
         key: apiKey,
@@ -17,8 +17,19 @@ function init() {
         tileClass: OpenLayers.Tile.ASCII
     });
 
+    var mapbox_streets_ascii = new OpenLayers.Layer.OSM("MapBox Streets", [
+        'http://a.tiles.mapbox.com/v3/mapbox.mapbox-streets/${z}/${x}/${y}.png',
+        'http://b.tiles.mapbox.com/v3/mapbox.mapbox-streets/${z}/${x}/${y}.png',
+        'http://c.tiles.mapbox.com/v3/mapbox.mapbox-streets/${z}/${x}/${y}.png',
+        'http://d.tiles.mapbox.com/v3/mapbox.mapbox-streets/${z}/${x}/${y}.png'
+    ], {
+        numZoomLevels: 17,
+        attribution: "<a href='http://mapbox.com/about/maps'>Terms &amp; Feedback</a>",
+        tileClass: OpenLayers.Tile.ASCII
+    });
+
     map = new OpenLayers.Map('map', {
-        layers: [osm_ascii, osm, bing_ascii],
+        layers: [mapbox_streets_ascii, osm_ascii, osm, bing_ascii],
         theme: null
     });
     map.addControl(new OpenLayers.Control.LayerSwitcher());
