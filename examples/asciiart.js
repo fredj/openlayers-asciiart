401b9e2f2c1d3046f97ace68b8be55495342f02b
Frederic Junod <frederic.junod@camptocamp.com>
2012-05-16 16:19:27 +0200

    Add GUI to change blockSize and gradient
    http://code.google.com/p/dat-gui/

--- examples/asciiart.js
+++ examples/asciiart.js
@@ -4,9 +4,24 @@ var map;
 // http://bingmapsportal.com/ and use that instead.
 var apiKey = "Am8aOpNMQEbMlrtBAkv_Q4vUETK6p8lVkY9D18hmLgHXY8C96DYUUQVa5k-Av8Or";
 
+var TileOptions = function() {
+    this.blockSize = 8;
+    this.gradient = '@80GCLft1i;:,. '.split('');
+};
+
+var gradients = [
+    '@80GCLft1i;:,. '.split(''),
+    '@8Oo:. '.split(''),
+    'MHb6j+:. '.split(''),
+    '"█▓▒░ '.split('')
+];
+
 function init() {
+    var tileOptions = new TileOptions();
+
     var osm_ascii = new OpenLayers.Layer.OSM("Mapnik ASCII", null, {
-        tileClass: OpenLayers.Tile.ASCII
+        tileClass: OpenLayers.Tile.ASCII,
+        tileOptions: tileOptions
     });
     var osm = new OpenLayers.Layer.OSM("Mapnik");
 
@@ -14,7 +29,8 @@ function init() {
         name: "Bing road ASCII",
         key: apiKey,
         type: "Road",
-        tileClass: OpenLayers.Tile.ASCII
+        tileClass: OpenLayers.Tile.ASCII,
+        tileOptions: tileOptions
     });
 
     var mapbox_streets_ascii = new OpenLayers.Layer.OSM("MapBox Streets", [
@@ -25,7 +41,8 @@ function init() {
     ], {
         numZoomLevels: 17,
         attribution: "<a href='http://mapbox.com/about/maps'>Terms &amp; Feedback</a>",
-        tileClass: OpenLayers.Tile.ASCII
+        tileClass: OpenLayers.Tile.ASCII,
+        tileOptions: tileOptions
     });
 
     map = new OpenLayers.Map('map', {
@@ -34,4 +51,24 @@ function init() {
     });
     map.addControl(new OpenLayers.Control.LayerSwitcher());
     map.zoomTo(2);
+
+    // controls init
+    var gui = new dat.GUI();
+
+    gui.add(tileOptions, 'blockSize', 1, 16).step(1)
+        .onFinishChange(function(value) {
+            update_tile_option(this.property, value);
+        });
+    gui.add(tileOptions, 'gradient', gradients)
+        .onFinishChange(function(value) {
+            update_tile_option(this.property, value);
+        });
+}
+
+function update_tile_option(key, value) {
+    for (var i = 0, len = map.layers.length; i < len; i++) {
+        map.layers[i].tileOptions[key] = value;
+        map.layers[i].clearGrid();
+        map.layers[i].redraw();
+    }
 }
